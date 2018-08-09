4a27e9d69ff6b21fe3ccb6964289a79efe7ff11a
Tyler Tracy <tylertracy1999@gmail.com>
2018-08-08 19:56:47 -0500

    fixed login issue

--- src/auth.ts
+++ src/auth.ts
@@ -2,7 +2,7 @@ import { FriendModel } from './components/friend/schema';
 
 const CheckPass = (req, res): void => {
   const { pass, whoami } = req.query;
-  FriendModel.findOne({ friend_d: whoami })
+  FriendModel.findOne({ friend_id: whoami })
     .then((friend) => {
       if (friend.hash === pass) {
         res.status(202).send('Sucess');
